731253666ec2b06a236f70fe789b96a1f55b6a2e
Jose Miguel Ascuy Anturiano <imigelose2@gmail.com>
2022-08-31 11:00:33 -0400

    primer commit

---
+++ app/src/main/java/com/example/frameyconstrain/ConstrainLayout.java
@@ -0,0 +1,14 @@
+package com.example.frameyconstrain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConstrainLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constrain_layout);
+    }
+}
